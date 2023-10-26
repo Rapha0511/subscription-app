48ffbd6143d1d876a083772c07f5690bbbb928ce
ThomasMms23 <monmousseau.thomas@gmail.com>
2023-10-26 17:16:24 +0200

    fix

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -3,67 +3,82 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Details() {
-  const [abonnement, setAbonnement] = useState([]);
+    const [abonnement, setAbonnement] = useState([]);
 
-  let { id } = useParams();
-  let navigate = useNavigate();
-  const fetchAbonnement = async () => {
-    const response = await fetch(`http://localhost:3000/abonnementsList/${id}`);
-    const res = await response.json();
-    setAbonnement(res);
-    // Filter the data when you fetch it initially
-  };
+    let { id } = useParams();
+    let navigate = useNavigate();
+    const fetchAbonnement = async () => {
+        const response = await fetch(
+            `http://localhost:3000/abonnementsList/${id}`
+        );
+        const res = await response.json();
+        setAbonnement(res);
+        // Filter the data when you fetch it initially
+    };
 
-  useEffect(() => {
-    fetchAbonnement();
-  }, []);
+    useEffect(() => {
+        fetchAbonnement();
+    }, []);
 
-  /**
-   *
-   * @param {Object} data the object to add
-   */
-  const addSub = async (data) => {
-    const response = await fetch("http://localhost:3000/abonnements", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    /**
+     *
+     * @param {Object} data the object to add
+     */
+    const addSub = async (data) => {
+        const response = await fetch("http://localhost:3000/abonnements", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
 
-    if (response.status === 201) {
-      console.log("success");
-      deleteFromList(data.id);
-      navigate("/");
-    } else {
-      console.log("error");
-    }
-  };
+        if (response.status === 201) {
+            console.log("success");
+            deleteFromList(data.id);
+            navigate("/");
+        } else {
+            console.log("error");
+        }
+    };
 
-  const deleteFromList = async (id) => {
-    const response = await fetch(
-      `http://localhost:3000/abonnementsList/${id}`,
-      {
-        method: "DELETE",
-      }
+    const deleteFromList = async (id) => {
+        const response = await fetch(
+            `http://localhost:3000/abonnementsList/${id}`,
+            {
+                method: "DELETE",
+            }
+        );
+
+        if (response.status === 204) {
+            console.log("error");
+        } else {
+            console.log("success");
+        }
+    };
+
+    return (
+        <div>
+            <div className="details__title">
+                <p className="details__name">
+                    {abonnement.nom}
+                    <br />
+                    <span className="details__type">{abonnement.type}</span>
+                </p>
+                <p className="details__carbon">{abonnement.carbon} CO2</p>
+            </div>
+            <div className="details__image">
+                <img src={"../" + abonnement.image} />
+            </div>
+            <p>Impact environnemental </p>
+            <p>Description de l'outil : {abonnement.description}</p>
+            <p>Actualités</p>
+            <button
+                className="details__button"
+                onClick={() => addSub(abonnement)}
+            >
+                Je me suis abonné
+            </button>
+        </div>
     );
-
-    if (response.status === 204) {
-      console.log("error");
-    } else {
-      console.log("success");
-    }
-  };
-
-  return (
-    <div>
-      <h1>Details de l'offre</h1>
-      <div>
-        <p>{abonnement.nom}</p>
-        <p>{abonnement.description}</p>
-        <p>{abonnement.prix}</p>
-      </div>
-      <button onClick={() => addSub(abonnement)}>Subscribe</button>
-    </div>
-  );
 }
