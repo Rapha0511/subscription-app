66358a8abf8096008e3200fad946af38e4b198b3
ThomasMms23 <monmousseau.thomas@gmail.com>
2023-10-26 23:57:52 +0200

    fix orto

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,23 +1,23 @@
 import { useState } from "react";
 
 export default function FilterBar({ filteringOptions, setFilteringOptions }) {
-  const [searchInput, setSearchInput] = useState("");
-  const getSearch = (e) => {
-    const { value } = e.target;
-    setSearchInput(value);
-    setFilteringOptions({ ...filteringOptions, name: value });
-  };
+    const [searchInput, setSearchInput] = useState("");
+    const getSearch = (e) => {
+        const { value } = e.target;
+        setSearchInput(value);
+        setFilteringOptions({ ...filteringOptions, name: value });
+    };
 
-  return (
-    <div>
-      <input
-        className="filter__bar"
-        type="text"
-        name="search"
-        placeholder="Trouver ou rajouter un abonnements"
-        value={searchInput}
-        onChange={getSearch}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <input
+                className="filter__bar"
+                type="text"
+                name="search"
+                placeholder="Trouver ou rajouter un abonnement"
+                value={searchInput}
+                onChange={getSearch}
+            />
+        </div>
+    );
 }
